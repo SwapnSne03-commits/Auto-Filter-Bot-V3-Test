96fa4ff4c1333bae005c4711db2a1c87a1adc6c5
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-02 16:34:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -52,6 +52,7 @@ class temp(object):
     IMDB_CAP = {}
     VERIFICATIONS = {}
     DAILY_USAGE = {} 
+    OWNER = ADMINS  # alias for backward compatibility
 
 def today_date():
     return datetime.now().strftime("%Y-%m-%d")
