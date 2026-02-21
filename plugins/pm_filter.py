622588e3744dd35374226b2af49e42ee506f1459
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-21 19:03:34 +0530

    Update pm_filter.py
    Non english request ignore

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2350,6 +2350,8 @@ async def auto_filter(client, msg, spoll=False):
             search = search.replace("'","")
             search = re.sub(r'[^a-zA-Z0-9\s]', '', search)
             search = re.sub(r'\s+', ' ', search).strip()
+            if not re.search(r'[a-zA-Z0-9]', search): #ignore non english request 
+                return
             m=await message.reply_text(f'<b>Wait {message.from_user.mention} Searching Your Query: <i>{search}...</i></b>', reply_to_message_id=message.id)
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
             uid = message.from_user.id if message.from_user else 0
