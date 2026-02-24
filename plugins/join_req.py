34ec5193522ba10e6d6eb3ee2325cb2acee2ee57
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-24 10:59:12 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -7,17 +7,25 @@ from setting import get_settings
 @Client.on_chat_join_request()
 async def join_reqs(client, message: ChatJoinRequest):
 
-    settings = await get_settings(message.chat.id)
-    req_channel = settings.get("req_fsub_id")
+    try:
+        channel_id = message.chat.id
 
-    if not req_channel:
-        return
+        # 🔹 সব group settings check করতে হবে
+        groups = await db.get_all_groups()  # তোমার db function অনুযায়ী adjust করো
 
-    if message.chat.id != req_channel:
-        return
+        for grp in groups:
+            settings = await get_settings(grp["group_id"])
+            req_channel = settings.get("req_fsub_id")
 
-    if not await db.find_join_req(message.from_user.id, message.chat.id):
-        await db.add_join_req(message.from_user.id, message.chat.id)
+            if req_channel and req_channel == channel_id:
+
+                if not await db.find_join_req(message.from_user.id, channel_id):
+                    await db.add_join_req(message.from_user.id, channel_id)
+
+                break
+
+    except Exception as e:
+        print(f"Join Req Error: {e}")
 
 @Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
 async def del_requests(client, message):
