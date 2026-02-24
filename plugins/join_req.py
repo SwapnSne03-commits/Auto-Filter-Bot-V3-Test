4c7db0dc4b081a04dbc7cb70a05c97d80440fdc5
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-24 13:22:30 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -11,25 +11,9 @@ async def join_reqs(client, message: ChatJoinRequest):
         channel_id = message.chat.id
         user_id = message.from_user.id
 
-        groups = await db.get_all_groups()
-
-        for grp in groups:
-            settings = await get_settings(grp["group_id"])
-            req_channels = settings.get("req_fsub_id")
-
-            if not req_channels:
-                continue
-
-            if not isinstance(req_channels, list):
-                req_channels = [req_channels]
-
-            if channel_id in req_channels:
-
-                # 🔹 prevent duplicate insert
-                if not await db.find_join_req(user_id, channel_id):
-                    await db.add_join_req(user_id, channel_id)
-
-                return  # stop after first match
+        # 🔹 Prevent duplicate insert
+        if not await db.find_join_req(user_id, channel_id):
+            await db.add_join_req(user_id, channel_id)
 
     except Exception as e:
         LOGGER.error(f"Join Req Error: {e}")
