93d4febee1c4b4843188c84694e4506779d46b6e
SwapnSne03-commits <somsneha03@gmail.com>
2026-02-24 09:47:06 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,12 +1,23 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database.users_chats_db import db
-from info import ADMINS, AUTH_REQ_CHANNEL
+from info import ADMINS
+from setting import get_settings
 
-@Client.on_chat_join_request(filters.chat(AUTH_REQ_CHANNEL))
+@Client.on_chat_join_request()
 async def join_reqs(client, message: ChatJoinRequest):
-  if not await db.find_join_req(message.from_user.id, message.chat.id):
-    await db.add_join_req(message.from_user.id, message.chat.id)
+
+    settings = await get_settings(message.chat.id)
+    req_channel = settings.get("req_fsub_id")
+
+    if not req_channel:
+        return
+
+    if message.chat.id != req_channel:
+        return
+
+    if not await db.find_join_req(message.from_user.id, message.chat.id):
+        await db.add_join_req(message.from_user.id, message.chat.id)
 
 @Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
 async def del_requests(client, message):
